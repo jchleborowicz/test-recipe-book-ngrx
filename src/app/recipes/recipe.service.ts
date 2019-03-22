b2910146a0bfac28663d85cd96d11f58f378d57e
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2019-03-22 16:20:31 +0100

    Initial commit.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Store} from '@ngrx/store';
 
-import { Recipe } from './recipe.model';
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -27,7 +29,9 @@ export class RecipeService {
       ])
   ];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService,
+              private shoppingListStore: Store<{ shoppingList: { ingredients: Ingredient[] } }>) {
+  }
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -43,7 +47,9 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
+    ingredients.forEach(ingredient => {
+      this.shoppingListStore.dispatch(new ShoppingListActions.AddIngredient(ingredient));
+    });
   }
 
   addRecipe(recipe: Recipe) {

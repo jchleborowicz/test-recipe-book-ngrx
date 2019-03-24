d1b1064f6159b5c3fd60a7d45f0fa0a74ef7772c
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2019-03-24 14:35:04 +0100

    Initial commit.

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Store} from '@ngrx/store';
 
 import * as fromApp from '../store/app.reducers';
@@ -8,12 +8,18 @@ import * as fromAuth from './store/auth.reducers';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private store: Store<fromApp.AppState>) {
+  constructor(private store: Store<fromApp.AppState>,
+              private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot) {
     return this.store.select('auth')
-      .map((authState: fromAuth.State) => authState.authenticated);
+      .map((authState: fromAuth.State) => {
+        if (!authState.authenticated) {
+          this.router.navigate(['/signin']);
+        }
+        return authState.authenticated;
+      });
   }
 }
